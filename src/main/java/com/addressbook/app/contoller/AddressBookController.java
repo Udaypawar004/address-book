daf94163f6f7a57a8bdc4a51a3664225c2f27a35
Uday Deshmukh <udaypawar004@gmail.com>
2025-02-28 14:57:13 +0530

    [Uday] Add. section1 uc2 all rest methods.

--- src/main/java/com/addressbook/app/contoller/AddressBookController.java
+++ src/main/java/com/addressbook/app/contoller/AddressBookController.java
@@ -1,14 +1,28 @@
 package com.addressbook.app.contoller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/app")
 public class AddressBookController {
-    @GetMapping
-    public String sayHii() {
-        return "Hey guys";
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<String> sayHii() {
+        return ResponseEntity.ok("heyy");
+    }
+
+    @PostMapping(value = "/{name}")
+    public ResponseEntity<String> postData(@PathVariable String name) {
+        return ResponseEntity.ok("Hey " + name + "!");
+    }
+
+    @PutMapping(value = "/{name}")
+    public ResponseEntity<String> putData(@PathVariable String name) {
+        return ResponseEntity.ok("Hey " + name + "!");
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<String> deleteData(@PathVariable String name) {
+        return ResponseEntity.ok("Hey " + name + "! \nWe are in delete mapping.");
     }
 }
