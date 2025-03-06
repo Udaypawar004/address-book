4b0393e970bb3975a9c12637da119d6ca1f57d6b
Uday Deshmukh <udaypawar004@gmail.com>
2025-03-06 16:14:23 +0530

    [Uday] Add. section3 uc3 prod and dev logging levels.

--- src/main/java/com/addressbook/app/AddressBookApplication.java
+++ src/main/java/com/addressbook/app/AddressBookApplication.java
@@ -3,14 +3,15 @@ package com.addressbook.app;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @Slf4j
 @SpringBootApplication
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		log.info("Application started...");
-		SpringApplication.run(AddressBookApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Application started in {} environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
