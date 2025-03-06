5fa0ff008f23736a1b5d887f0447acb05e2a9f43
Uday Deshmukh <udaypawar004@gmail.com>
2025-03-06 16:15:57 +0530

    [Uday] Add. section3 uc4 DB user logging.

--- src/main/java/com/addressbook/app/AddressBookApplication.java
+++ src/main/java/com/addressbook/app/AddressBookApplication.java
@@ -12,6 +12,7 @@ public class AddressBookApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
 		log.info("Application started in {} environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
